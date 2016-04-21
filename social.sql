5f194352210f0854399c3f41b9404162650a5de3
unknown <stud_17@17_14.efdnew.isuct.ru>
2016-04-21 16:41:13 +0400

    social

---
+++ social.sql
@@ -0,0 +1,5 @@
+10. SELECT distinct name FROM Highschooler WHERE ID IN (SELECT ID1 FROM Friend WHERE ID2 IN (SELECT ID FROM Highschooler WHERE name="Gabriel"));
+11. select distinct sName, sGrade, lName, lGrade from (select h1.name as sName, h1.grade sGrade, h2.name as lName, h2.grade as lGrade, h1.grade-h2.grade as gradeDiff  from Highschooler h1, Likes, Highschooler h2 where h1.ID=ID1 and h2.ID=ID2) where gradeDiff>=2;
+12. select h1.name, h1.grade, h2.name, h2.grade  from Likes l1, Likes l2, Highschooler h1, Highschooler h2 where l1.ID1=l2.ID2 and l2.ID1=l1.ID2 and l1.ID1=h1.ID and l1.ID2=h2.ID and h1.name<h2.name;
+13. select name, grade from Highschooler where ID in (select ID1 from Highschooler H1, Friend, Highschooler H2 where H1.ID = Friend.ID1 and Friend.ID2 = H2.ID and H1.grade <> H2.grade)order by grade, name;
+14. select name, grade from (select ID2, count(ID2) as numLiked from Likes group by ID2), Highschooler where numLiked>1 and ID2=ID;
