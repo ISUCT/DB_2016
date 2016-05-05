3e225f7f91e3eab73327977cf15b4d521097e6c1
Your Name <you@example.com>
2016-05-05 16:32:03 +0400

    sgl

---
+++ modification.sql
@@ -0,0 +1,13 @@
+1) insert into Reviewer
+   values (209, 'Roger Ebert');
+2) insert into Rating  
+  select Rating.rID , Movie.mID, 5 as stars, null as ratingDate
+  from Rating,Movie, Reviewer
+   where Rating.rID = Reviewer.rID
+   and Reviewer.name = 'James Cameron' ;
+ 
+
+3)update Movie set year=year+25 where
+  mID in (select mID from (select Rating.mID,avg(stars) as average  
+  from Movie, Rating where Movie.mID=Rating.mID 
+  group by Rating.mID, year) where average>=4);
