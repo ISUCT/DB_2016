99494d0a1ab8f6e93fbe2859cbaadd3be53a4bcb
Allevy <123ami123@mail.ru>
2016-05-06 09:38:24 +0300

    Mod complete

---
+++ mod.sql
@@ -0,0 +1,24 @@
+/*Q1
+insert into Reviewer(rID, name) values (209, 'Roger Ebert');
+
+/*Q2
+insert into Rating  ( rID, mID, stars, ratingDate )
+select Reviewer.rID , Movie.mID, 5, null from Movie
+left outer join Reviewer
+where Reviewer.name='James Cameron';
+
+/*Q3
+update movie
+set year = year + 25
+where mID in (
+  select mID from (
+  select AVG(stars) as astar, mID from Rating
+  where mID=rating.mID
+  group by mID
+  having astar >=4)
+);
+
+/*Q4
+delete from rating
+where mID in (select mID from movie where year <1970 or year > 2000)
+and stars < 4; 
