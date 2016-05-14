9c1bf50c04faa3b90466981c77d3e01074608f4e
user <you@example.com>
2016-05-14 09:36:03 +0400

    SOCIAL MODIFICATIONS.sql

---
+++ SOCIAL MODIFICATIONS.sql
@@ -0,0 +1,10 @@
+1) delete from Highschooler
+  where grade =12
+  
+2) delete from likes
+   where id1 in (select likes.id1 
+   from friend join likes using (id1) 
+   where friend.id2 = likes.id2) 
+   and not id2 in (select likes.id1 
+   from friend join likes using (id1) 
+   where friend.id2 = likes.id2)
