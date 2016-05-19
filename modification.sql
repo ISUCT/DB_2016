bcf827beedb4badb50414e48b06a6497f5e00766
Ирина Кузнецова < >
2016-05-19 23:08:47 +0300

    modification

--- modification.sql
+++ modification.sql
@@ -1,9 +1,24 @@
-1) insert into Reviewer(rID, name) values (209, 'Roger Ebert');
+1) insert into Reviewer(rID, name) 
+   values (209, 'Roger Ebert');
 
 2) insert into Rating
-   select R.rID,M.mID,5,NULL from (Select rID from Reviewer where name = 'James Cameron') R,
+   select R.rID,M.mID,5,NULL 
+   from (Select rID from Reviewer where name = 'James Cameron') R,
    (select mID from Movie) M;
 
-3) 
+3) update Movie
+   set year = year + 25
+   where mID in (
+   select Movie.mId
+   from Movie, Rating
+   where Movie.mID = Rating.mID
+   group by Movie.mID
+   having avg(stars) >= 4);
 
-4)
+4) delete from Rating
+   where mID in (
+   select distinct Rating.mID
+   from Movie, Rating
+   where Movie.mID = Rating.mID
+   and (Movie.year > 2000 or Movie.year < 1970)
+   and stars < 4;
