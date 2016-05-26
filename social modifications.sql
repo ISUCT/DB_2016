21eb708456979ab9545f22e8fda1d3e2ac6409c8
unknown <Stud_17@17_04.efdnew.isuct.ru>
2016-05-26 17:32:55 +0400

    socialmodification

--- social modifications.sql
+++ social modifications.sql
@@ -9,3 +9,10 @@ where friend.id2 = likes.id2)
 and not id2 in (select likes.id1 
 from friend join likes using (id1) 
 where friend.id2 = likes.id2);
+
+3) insert into friend
+select f1.id1, f2.id2
+from friend f1 join friend f2 on f1.id2 = f2.id1
+where f1.id1 <> f2.id2
+except
+select * from friend;
