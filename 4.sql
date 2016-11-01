102f9e59df1151a4ba110bac154fdcf0c16c5f33
dope11 <miss.rizzle@yandex.ru>
2016-11-01 14:14:22 +0400

    Add files via upload

---
+++ 4.sql
@@ -0,0 +1,9 @@
+﻿CREATE Function del_purchase() returns trigger as $$
+BEGIN
+if (select count(*) from purchase where purchase.ID_Client=OLD.ID_Client)>0
+then
+delete from purchase where purchase.ID_Client=OLD.ID_Client;
+end if;
+return OLD;
+END;
+$$LANGUAGE plpgsql;
