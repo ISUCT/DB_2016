b47398683517ab3f16ad38a8623754d66c7c5c1d
dope11 <miss.rizzle@yandex.ru>
2016-11-15 13:00:55 +0400

    Add files via upload

---
+++ 1.sql
@@ -0,0 +1,39 @@
+﻿CREATE TABLE Client
+(
+ID_Client Int primary key,
+Name varchar,
+Phone_number Int,
+Address varchar
+);
+
+CREATE TABLE Book
+(
+ID_Book Int primary key,
+Name varchar,
+Author varchar,
+Section_of_literature varchar,
+Price Int
+);
+
+CREATE TABLE Author
+(
+ID_Author Int primary key,
+Name varchar
+);
+
+CREATE TABLE Cart
+(
+ID_Order Int primary key,
+ID_Book Int,
+Quantity Int
+);
+
+CREATE TABLE Purchase
+(
+ID_Order Int primary key,
+ID_Client Int,
+Order_Date TimeStamp,
+Delivery_Date TimeStamp,
+Shipping_Cost Int,
+Delivery_Type varchar
+);
